28555a00b76b234afee5cb46493a2cfa82596ad7
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-03 12:07:00 +0900

    Initial Commit

---
+++ FrontendMentor00_SwiftUI/FrontendMentor00_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendMentor00_SwiftUIApp.swift
+//  FrontendMentor00_SwiftUI
+//
+//  Created by Jinwook Kim on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct FrontendMentor00_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
